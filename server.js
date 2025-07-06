274c34d3a5e643c5910228f6b056c80786df0821
sanaatawalbeh <sanaatawalbeh0@gmail.com>
2025-07-06 22:49:06 +0300

    port edited

--- server.js
+++ server.js
@@ -9,8 +9,6 @@ const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
 
 const jwt = require("jsonwebtoken");
 
-
-
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
@@ -22,7 +20,6 @@ const auth = require("./routes/auth");
 app.use("/", homeRouter);
 app.use("/recipes", recipesRouter);
 app.use("/user", auth);
-
 
 // -----------------------------------------------------------------------------------------------
 const port = process.env.PORT || 3000;
@@ -52,24 +49,10 @@ pool
     console.error("Could not connect to database:", err);
   });
 
-  app.get("/user/me", (req, res) => {
-    const authHeader = req.headers["authorization"];
-    if (!authHeader) {
-      return res.status(401).json({ message: "No token provided" });
-    }
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
 
-    const token = authHeader.split(" ")[1];
-
-    try {
-      const decoded = jwt.verify(token, env.process.JWT_SECRET); // بدّل SECRET_KEY بالمفتاح الحقيقي
-      res.json({
-        username: decoded.username,
-        email: decoded.email || "test@example.com",
-      });
-    } catch (err) {
-      return res.status(403).json({ message: "Invalid token" });
-    }
-  });
 // -----------------------------------------------------------------------------------------------
 app.use((req, res) => {
   res.status(404).send('Page not found <a href="/"> Get back home</a>');
